356b6b6096cec400acf6090c9fcc03e3c8fee9fa
rsamidev <samina.rahmanyar@uoit.net>
2017-03-12 17:14:03 -0400

    Added Validity Check part
    slight modification so that it follows requirements

--- sudoku.h
+++ sudoku.h
@@ -10,6 +10,9 @@ Improvements
 #include <stdbool.h>
 
 static int puzzle[9][9]; 
+int col_threads[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int row_threads[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int box_threads[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 void initialize_puzzle(){ //convert puzzle.txt to a 2-D array
     FILE *f;
@@ -31,12 +34,14 @@ void show_array_test(){ //Testing purposes prints array as stored
             printf("%d ", puzzle[i][j]); 
         }
     }
+    printf("\n");//needed this
 }
 
 
-void check_row(int i){
-int arr[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};
-int cc = 0;
+void *check_row(void *x){
+    int i = *((int*)x);
+    int arr[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int cc = 0; 
 
     for (int j = 0; j <= 8; j++){ //for each entry in the row
             for (int c = 0; c <= 8; c++){//for all values in the row
@@ -44,45 +49,67 @@ int cc = 0;
                     break;
                 }
                 else if (puzzle[i][j] != arr[c]){ //Current value doesnt already exist in array
-                    printf("Test: arr[%d] entry %d is valid\n", c, puzzle[i][j]);
+                    //printf("Test: arr[%d] entry %d is valid\n", c, puzzle[i][j]);
                 }
                 else{ //number was already in the row
-                    printf("There are multiple %d's in row %d\n",puzzle[i][j], i);
+                 //   printf("There are multiple %d's in row %d\n",puzzle[i][j], i);
                     return; //Ends loop if an issue is found
                 }
             } 
-            printf("\nEntry %d is valid\n", puzzle[i][j]);
+           // printf("\nEntry %d is valid\n", puzzle[i][j]);
                 arr[cc] = puzzle[i][j];
                 cc++; 
-    } 
+    }
+    row_threads[i] = 1; 
 }
 
 
 
-void check_column(int j){
+void *check_column(void *x){
+    int j = *((int*)x);
+
     int arr[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};
     int cc = 0;
 
     for (int i = 0; i <= 8; i++){ //for each entry in the row
             for (int c = 0; c <= 8; c++){//for all values in the row
                 if (puzzle[i][j] == 0){ //0 represents empty box, should not be tested
+                //    printf("Space %d is empty, skip\n", cc);
                     break;
                 }
                 else if (puzzle[i][j] != arr[c]){ //Current value doesnt already exist in array
-                    printf("Test: arr[%d] entry %d is valid\n", c, puzzle[i][j]);
+                  //  printf("Test: arr[%d] entry %d is valid\n", c, puzzle[i][j]);
                 }
                 else{ //number was already in the row
-                    printf("There are multiple %d's in column %d\n",puzzle[i][j], i);
+                   // printf("There are multiple %d's in column %d\n",puzzle[i][j], i);
                     return; //Ends loop if an issue is found
                 }
             } 
-            printf("Entry %d is valid\n\n", puzzle[i][j]);
+          //  printf("Entry %d is valid\n\n", puzzle[i][j]);
                 arr[cc] = puzzle[i][j];
                 cc++; 
     } 
+    col_threads[j] = 1;
+   // printf("Column %d is valido\n", j);
+    
 }
 
-void check_box(int boxno){
+void check_all(){
+    for (int j=0; j<9; j++)
+        {
+            if (!col_threads[j] || !row_threads[j] || !box_threads[j]) //if anything has an invalid entry
+            {
+            printf("INVALID\n");
+            return;
+            }
+        }
+    printf("VALID\n"); //if we are still running, we havent recieved any invalid entries
+}
+
+
+void *check_box(void *x){
+    int boxno = *((int*)x);
+
     int arr[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0};
     int cc = 0;
     int i = boxno/3 *3;
@@ -95,19 +122,20 @@ void check_box(int boxno){
                     break;
                 }
                 else if (puzzle[i+crow][j+ccol] != arr[c]){ //Current value doesnt already exist in array
-                    printf("Test: arr[%d] entry %d is valid\n", c, puzzle[i+crow][j+ccol]);
+                //    printf("Test: arr[%d] entry %d is valid\n", c, puzzle[i+crow][j+ccol]);
                 }
                 else{ //number was already in the row
 
-                    printf("There are multiple %d's in box %d\n",puzzle[i+crow][j+ccol], boxno);
+                  //  printf("There are multiple %d's in box %d\n",puzzle[i+crow][j+ccol], boxno);
                     return; //Ends loop if an issue is found
                 }
             } 
-              printf("Entry %d is valid\n\n", puzzle[i+crow][j+ccol]);
+              //printf("Entry %d is valid\n\n", puzzle[i+crow][j+ccol]);
                 arr[cc] = puzzle[i+crow][j+ccol];
                 cc++;  
         }           
-    } 
+    }
+    box_threads[boxno] = 1; 
 
 }
 
